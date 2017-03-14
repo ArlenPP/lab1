b17eebc36d063bee17dfa59cafaf776178a4e0e7
ArlenPP <htoatot@gmail.com>
2017-03-14 14:56:35 +0800

    add homework question b

---
+++ A.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+void function(int n){
+	cout << n;
+	while(n!=1){
+		if (n%2==1)
+			n = 3*n+1;
+		else n = n/2;
+		cout << ' ' << n ;
+	}
+}
+
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	while(scanf("%d",&n)!=EOF){
+		function(n);
+		cout << endl;
+	}
+	return 0;
+}
+
+
